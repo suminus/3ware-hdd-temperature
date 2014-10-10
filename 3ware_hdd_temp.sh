c6c57baf0fafa0cfa7abe4c454f331b560ac465b
suminus <office@mariohartz.de>
2014-10-10 22:09:22 +0200

    Update 3ware_hdd_temp.sh

--- 3ware_hdd_temp.sh
+++ 3ware_hdd_temp.sh
@@ -6,7 +6,6 @@ CONTR=$( tw_cli show | grep c | cut -d " " -f1 | sed 's/^/\//' | tr '\n' ' ' )
 for CON in  $CONTR
 do
 PORT=$( /usr/sbin/tw_cli ${CON} show | cut -d " " -f1 | grep p )
-
         for POR in $PORT
         do
         /usr/sbin/tw_cli ${CON}/${POR} show all | grep Temp
