367ad53269f511d8b27df103201d0e4b44c698f7
mario hartz <office@mariohartz.de>
2014-10-10 10:06:46 +0200

    3ware_hdd_temp.sh

---
+++ 3ware_hdd_temp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONTR=$( tw_cli show | grep c | cut -d " " -f1 | sed 's/^/\//' | tr '\n' ' ' )
+
+for CON in  $CONTR
+do
+PORT=$( /usr/sbin/tw_cli ${CON} show | cut -d " " -f1 | grep p )
+
+        for POR in $PORT
+        do
+        /usr/sbin/tw_cli ${CON}/${POR} show all | grep Temp
+        done
+
+done
+
+exit
